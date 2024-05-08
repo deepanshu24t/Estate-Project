1e1e38eff41b98ae70a682de4f3db3a6f581711e
deepanshu24t <deepanshugupta243@gmail.com>
2024-05-08 21:16:40 +0530

    some changes in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ function Header() {
         </h1>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Search' className='bg-transparent focus:outline-none w-24 sm:w-64 text-xl' />
-            <FaSearch className='text-slate-600 text-xl'/>
+            <FaSearch className='text-slate-600 text-xl '/>
         </form>
-        <ul className='flex gap-6 text-xl text-slate-800 cursor-pointer '>
+        <ul className='flex gap-6 text-xl text-black cursor-pointer '>
           <Link to = "/"><li className='hidden sm:inline '>Home</li></Link>  
           <Link to = "/about">  <li className='hidden sm:inline'>About</li></Link>
           <Link to = "/sign-in">  <li>Login</li></Link>
