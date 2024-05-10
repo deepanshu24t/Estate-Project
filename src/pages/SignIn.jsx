4e4c46a4956b9e3285df49f6969bd458863427e7
deepanshu24t <deepanshugupta243@gmail.com>
2024-05-10 21:32:47 +0530

    create signin component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 function SignIn() {
+
+  const [formData,setFormData]= useState({})
+  
+  const navigate = useNavigate()
+  const handleChange = (e)=>
+    {
+        setFormData({
+          ...formData,[e.target.id]:e.target.value,
+        })
+    }
+     const handleSubmit = async (e)=>
+      {
+             e.preventDefault();
+            // console.log('form submitted');
+            const res = await fetch('/api/auth/signin',{
+              method:'POST',
+              headers:{
+                'Content-Type':'application/json',
+              },
+              body:JSON.stringify(formData)
+            });
+            const data = await res.json();
+            navigate('/');
+            console.log(data);
+      }
+   // console.log(formData);
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Login</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+        <input type="email" name="" id="email" placeholder='Enter Email' className='border p-3 rounded-lg'onChange ={handleChange} />
+        <input type="password" name="" id="password" placeholder='Enter Password' className='border p-3 rounded-lg'onChange ={handleChange} />
+        <button className='bg-slate-700 text-white text-xl rounded-lg uppercase p-3 hover:opacity-95 disabled:opactiy-80'>Login</button>
+      </form>
+      <div className='flex flex-row gap-2 mt-1.5'>
+        <p className='font-semibold'>Don't have an account?</p>
+        <Link to ="/sign-up">
+       <span className='text-blue-700 font-semibold'>Sign Up</span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
